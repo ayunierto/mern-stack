5a08b323a83872965386a873df7fde20d3317d0a
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2022-08-23 06:32:50 -0500

    configuring server express

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+// Settings
+app.set('port', process.env.PORT || 4000);
+
+// Middlewares
+app.use(cors());
+app.use(express.json()); //
+
+// Routes
+app.get('/api/users', (req, res) => res.send('Users route'));
+app.get('/api/notes', (req, res) => res.send('Notes route'));
 
 module.exports = app;
